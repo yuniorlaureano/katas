68a5117b9a8c6feae0945d1807dd871e2cc30fd9
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2018-09-06 15:45:42 -0400

    cambio en la logica

--- Build Tower.js
+++ Build Tower.js
@@ -1,38 +1,43 @@
 function towerBuilder(nFloors) {
     
-    var towers = []; //represent the tower
-    var tower = ""; //hold the asterisk without space
-    var count = nFloors; //the number of time to iterate
-    var index = 1; //Represent the amount of asterisk a floor will have
-    var backSpace = ""; //represent the space in the back part of the floor
-    var frontSpace = ""; //represent the space in the front part of the floor
-    var briksCount = (nFloors != 1 ? nFloors + 2 : 1 ); //The amount of brick, the first floor has
-    var numberOfSpace = 0; // the number of space in each side of a floor
+    var floors = []; 
+    var tower = ""; 
+    var count = nFloors; 
+    var backSpace = "";
+    var frontSpace = "";
+    var lastFloor = 0;
+    var nAstericks = 0;
+    var nSpaces = 0;
+    var astericks = "";
     
-    while(count != 0)
-    {
-        for(var i =0; i < index; i ++  )
-        {
-            tower += "*";
-        }
+    
+    lastFloor = nFloors + (nFloors -1);
+    nAstericks = 1;
+  
+    for(var i = 1; i <= count; i++){
+      
+      nAstericks = (i == 1? 1 : nAstericks + 2);
+      nSpaces = lastFloor - nAstericks;
+      
+      for(var j = 0; j < (nSpaces / 2); j++){
         
-        numberOfSpace = Math.floor((briksCount - index) / 2);
-        for(var i = 0; i < numberOfSpace; i++)
-        {
-          backSpace += " ";
-          frontSpace += " ";
-        }
+        backSpace += " ";
+        frontSpace += " ";
+      }
+      
+      for(var j = 0; j < nAstericks; j++){
         
-        tower =  frontSpace + tower + backSpace;
-        
-        towers.push(tower);
-        tower = "";
-        index += 2;
-        frontSpace = "";
-        backSpace = "";
-        
-        count -= 1;
+        astericks += "*";
+      }
+      
+      floors.push(backSpace + astericks +frontSpace);
+      
+      astericks = "";
+      backSpace = "";
+      frontSpace = "";
+      tower = "";
     }
     
-    return towers;
+    
+    return floors;
 }
